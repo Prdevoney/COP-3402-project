375c5df6999b0414513b53e768eb53ce2f6aad15
rdiorio <116819759+rdiorio@users.noreply.github.com>
2023-09-11 01:29:11 -0400

    Scew sleepgit pull But I understand what we have to do now :)

--- vm.c
+++ vm.c
@@ -7,18 +7,50 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+ARRAY_SIZE 512;
 
-int newFunction(int num); 
+/*
+    You will never be given an input file with more than text length greater 
+    than 150 lines of code
+*/
 
-int main() {
-    printf("Hello, World!\n");
-    printf("how do you do"); 
-    int num = 5; 
-    newFunction(num); 
+int main(int argc char *argv[]) {
+
+    FILE *inputFile = fopen(argv[1], "r");
+    // use buffer thingy?? or simple fscanf stuff!!! :)
+    fscanf(file, "%d %d", &variable, &variable);
+    
+    // like ^^ that better probably ^ 
+    // links below
+    // -->  https://users.cs.utah.edu/~germain/PPS/Topics/C_Language/file_IO.html 
+    // --> https://www.opentechguides.com/how-to/article/c/45/c-read-file.html 
+    char buff[buffersize?];
+    fgets(buff, buff size-1, file);
+    /*  First you must load
+        the input program into the PAS. 
+    */ 
+
+    // Each PAS is always initialized to zero
+
+    /* 
+        The values of SP and BP will be set up dynamically 
+        once the program had been uploaded.
+    */
+
+    //int BP = the index immediately following the M value from the last instruction in the program
+
+    int text [ARRAY_SIZE];
+
+    int SP = BP - 1;
+    int PC = 0;
+    // int IR = idk
+
+    // printing out form array:
+    printf(text[0]);
+
+    fclose(inputFile);
 }
 
-int newFunction(int num){
-    printf("%d", num);
-}
 
