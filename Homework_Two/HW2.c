998c7611252a2c801951494934213dedf7d62e89
prdevoney <prdevo@gmail.com>
2023-09-21 21:17:36 -0400

    comment & include

--- Homework_Two/HW2.c
+++ Homework_Two/HW2.c
@@ -1,3 +1,13 @@
+/*
+    Patrick DeVoney & Rossella Diorio.
+    Homework Assignment Two: Lexical Analyzer.
+    Prof: Euripides Montage 
+    COP 3402, Fall 2023.
+*/
+
+#include <stdio.h>
+#include <stdlib.h> 
+
 typedef enum { 
     skipsym = 1, identsym, numbersym, plussym, minussym,
     multsym,  slashsym, ifelsym, eqsym, neqsym, lessym, leqsym,
@@ -9,8 +19,21 @@ typedef enum {
 
 
 int main(){
+
+    char * resWords [] = {"const", "var", "procedure", 
+                        "call", "begin", "end", "if", 
+                        "then", "ifel", "else", "while", 
+                        "do", "read", "write"};
+
+    int wsym [] = {constsym, varsym, procsym, callsym, 
+                    beginsym, endsym, ifsym, thensym, 
+                    ifelsym, elsesym, whilesym, dosym, 
+                    readsym, writesym};
+
+    int ssym [256];
+    ssym
     // loops over all input. 
-    while(prevToken != end (22) token != . (19)){
+    while(){
 
     }
 }
