e4f50422786eb244dc3f26e1aa5cc57af16ee463
prdevoney <prdevo@gmail.com>
2023-09-21 20:30:59 -0400

    main

---
+++ Homework_Two/HW2.c
@@ -0,0 +1,16 @@
+typedef enum { 
+    skipsym = 1, identsym, numbersym, plussym, minussym,
+    multsym,  slashsym, ifelsym, eqsym, neqsym, lessym, leqsym,
+    gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+    periodsym, becomessym, beginsym, endsym, ifsym, thensym, 
+    whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+    readsym , elsesym
+} token_type;
+
+
+int main(){
+    // loops over all input. 
+    while(prevToken != end (22) token != . (19)){
+
+    }
+}
